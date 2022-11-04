597f81ef99a9db3089c06e6d3a11a5e4c9109cff
Vladimir <71443706+27474129@users.noreply.github.com>
2022-11-04 16:38:26 +0700

    Delete pizzatime/api directory

--- pizzatime/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import *
-
-
-urlpatterns = [
-    path("get_order/", GetOrder.as_view()),
-    path("order_status_changing/", OrderStatusChanging.as_view()),
-]
